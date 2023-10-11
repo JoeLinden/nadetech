779a95196632a02541088d0e7c38d100e0bb320b
JoeLinden <j.linden123@gmail.com>
2023-10-10 21:01:35 -0500

    Add map icons and change font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
 import './globals.css'
-//import { Inter } from 'next/font/google'
+import { Kanit } from 'next/font/google'
 
-//const inter = Inter({ subsets: ['latin'] })
+const kanit = Kanit({
+  weight:  ['300', '500'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Nade Tech',
@@ -10,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={kanit.className}>
       <body>
         {children}
       </body>
