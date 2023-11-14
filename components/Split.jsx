feb9e143775987cbe660d88cbb1f36e13f9beb24
JoeLinden <j.linden123@gmail.com>
2023-11-13 19:33:31 -0600

    Fix deployment

--- components/Split.jsx
+++ components/Split.jsx
@@ -4,7 +4,7 @@ import Sidebar from "/components/Sidebar";
 import Content from "/components/Content";
 import SidebarData from "./SidebarData.js";
 import { useState } from "react";
-import { Nades } from "./Nades.js";
+import { Nades } from "./nades.js";
 
 const alias = {
   molotov: "molly",
